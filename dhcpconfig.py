2bf50c8b302544ab9d3e5304537c8f06e55314e1
Marco von Rosenberg <marcovr@selfnet.de>
2022-10-09 13:28:19 +0200

    Update DHCP config periodically

--- dhcpconfig.py
+++ dhcpconfig.py
@@ -10,40 +10,19 @@ class DnsmasqDhcpConfigGenerator():
             "port=0",  # Disable DNS server
             f"interface={self.interface}",
             "dhcp-option=3,0.0.0.0",  # No gateway / router
-            "dhcp-ignore=tag:!known"  # Ignore all devices not specified below using dhcp-host
+            "dhcp-ignore=tag:!known",  # Ignore all devices not specified below using dhcp-host
+            "dhcp-range=192.168.0.5,192.168.0.250,2m"
         ]
 
         for sw in switches:
             # Example: sftp://switch:SuperSecretPassword@192.168.0.1/ar-26a-2.cfg
-            sftp_path = f"sftp://{self.sftp_user}:{self.sftp_pass}@{self.sftp_ip}/{sw['name']}.cfg"
+            sftp_path = f"sftp://{self.sftp_user}:{self.sftp_pass}@{self.sftp_ip}/{sw.name}.cfg"
 
-            config_lines.append(f"\n######### Switch: {sw['name']} ({sw['mac']}) #########")
-            # Give this switch its specified IP-address
-            config_lines.append(f"dhcp-host={sw['mac']},{sw['ip']}")
-            # Creates a named "MAC adress filter"
-            config_lines.append(f"dhcp-host={sw['mac']},set:{sw['name']}")
+            config_lines.append(f"\n######### Switch: {sw.name} ({sw.mac}) #########")
+            # Give this switch its specified IP-address and creates a named "MAC adress filter"
+            config_lines.append(f"dhcp-host={sw.mac},{sw.ip},set:{sw.name}")
             # Only send the DHCP option to this specific device
-            config_lines.append(f"dhcp-option=set:{sw['name']},67,{sftp_path}")
+            config_lines.append(f"dhcp-option=set:{sw.name},67,{sftp_path}")
         
-        config_lines.append("")  # Newline at the end of the config file
+        config_lines.append("")
         return "\n".join(config_lines)
-
-
-if __name__ == "__main__":
-    confgen = DnsmasqDhcpConfigGenerator(interface="enp0s31f6", sftp_user="switch",
-        sftp_pass="SuperSecretPassword", sftp_ip="192.168.0.1")
-    
-    switches = [
-        {
-            "mac": "b0:22:7a:e6:91:34",
-            "name": "ar-26a-2",
-            "ip": "192.168.0.10"
-        },
-        {
-            "mac": "00:00:00:00:00:00",
-            "name": "testswitch",
-            "ip": "192.168.0.222"
-        },
-    ]
-
-    print(confgen.generate_config(switches))
---
+++ status_config_loop.py
@@ -0,0 +1,55 @@
+import db
+import asyncio
+from db import Session, Switch, SwitchStatus
+from dhcpconfig import DnsmasqDhcpConfigGenerator
+
+async def main():
+    #db.add_switch("b0:22:7a:e6:91:34")
+    #db.name_last_added_switch("ar3-26a-2")
+
+    config_gen = DnsmasqDhcpConfigGenerator(
+        interface="enp0s20f0u5",
+        sftp_user="switch",
+        sftp_pass="SuperSecretPassword",
+        sftp_ip="192.168.0.1"
+    )
+
+    dnsmasq_process = None
+
+    while True:
+        switches = db.query_all_unfinished_switches()
+        dnsmasq_config = config_gen.generate_config(switches)
+        with open("dnsmasq.conf", "w") as f:
+            f.write(dnsmasq_config)
+        
+        if dnsmasq_process is not None:
+            await (await asyncio.create_subprocess_exec("sudo", "killall", "dnsmasq")).wait()
+            await dnsmasq_process.wait()
+            output = (await dnsmasq_process.stderr.read(1024)).decode("utf-8")
+            lines = output.splitlines()
+            ack_lines = [l for l in lines if "DHCPACK" in l]
+
+            for line in ack_lines:
+                mac = line.split()[3]
+                with Session() as session:
+                    sw = session.query(Switch).filter(Switch.mac == mac).one()
+                    if sw.status < SwitchStatus.DHCP_SUCCESS:
+                        sw.status = SwitchStatus.DHCP_SUCCESS
+                    session.commit()
+
+            if len(ack_lines) > 0:
+                print(ack_lines)
+        
+        dnsmasq_process = await asyncio.create_subprocess_exec(
+            "sudo", "dnsmasq", "--no-daemon", "--conf-file=dnsmasq.conf",
+            stdin=asyncio.subprocess.DEVNULL,
+            stdout=asyncio.subprocess.DEVNULL,
+            stderr=asyncio.subprocess.PIPE
+        )
+
+        await asyncio.sleep(5)
+
+
+if __name__ == "__main__":
+    db.init_db()
+    asyncio.run(main())
