66dc4975a7a1948e41b821a4388d6fe744dc7212
Marco von Rosenberg <marcovr@selfnet.de>
2022-11-01 21:11:28 +0100

    Semi-good improvement of syslog parsing

--- syslog.py
+++ syslog.py
@@ -1,46 +1,53 @@
 import re
 
-def _get_time_from_syslog_line(line):
+def get_time_from_syslog_line(line):
     time_contained = line.split(";")[0]
     time_ = re.match(r"<.*>(.*) HUAWEI", time_contained, re.DOTALL).groups()[0]
     return time_
 
-def _parse_syslog_line_userdefined(line):
+def parse_syslog_line_userdefined(line):
     msg_text = line.split("; ")[1].split("(")[0]
     return msg_text
 
-def _parse_syslog_line_error(line):
+def parse_syslog_line_error(line):
     tags = re.findall(r"<error-tag>(.*)</error-tag>", line)
     error_msgs = re.findall(r"<error-message.*>(.*)</error-message>", line)
 
-    return f"{tags[0]} | {error_msgs[0]}"
+    return f"ERROR: {tags[0]} | {error_msgs[0]}"
+
+def parse_syslog_line_ssh_disconnect(line):
+    msg = line.split(";")[1].split("(")[0]
+    reason = re.findall(r"(Reason=.*)\)", line)[0].strip()
+    return f"{msg} | {reason}"
 
 def get_errors_from_syslog_lines(lines):
     errors = []
     for line in lines:
-        if "<error>" in line:
-            try:
-                time_ = _get_time_from_syslog_line(line)
-                errors.append(f"{time_} | {_parse_syslog_line_error(line)}")
-            except:
-                errors.append(line)
-    
+        try:
+            time_ = get_time_from_syslog_line(line)
+            if "<error>" in line:
+                errors.append(f"{time_} | {parse_syslog_line_error(line)}")
+            elif "SSHC_DISCONNECT" in line:
+                errors.append(f"{time_} | {parse_syslog_line_ssh_disconnect(line)}")
+        except:
+            errors.append(line)
+
     if len(errors) == 0:
         return ["No errors."]
-    
+
     return errors
 
 def get_infos_from_syslog_lines(lines):
     infos = []
     for line in lines:
-        if "OPS_LOG_USERDEFINED_INFORMATION" in line:
-            try:
-                time_ = _get_time_from_syslog_line(line)
-                infos.append(f"{time_} | {_parse_syslog_line_userdefined(line)}")
-            except:
-                infos.append(line)
-    
+        try:
+            if "OPS_LOG_USERDEFINED_INFORMATION" in line:
+                time_ = get_time_from_syslog_line(line)
+                infos.append(f"{time_} | {parse_syslog_line_userdefined(line)}")
+        except:
+            infos.append(line)
+
     if len(infos) == 0:
         return ["No log entries."]
-    
+
     return infos
