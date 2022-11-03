b32bf7e9b4ec228309d5ab36db009e31feb748d0
Marco von Rosenberg <codingmarco@gmail.com>
2022-11-03 23:47:54 +0100

    Various bugfixes

--- add_ips.py
+++ add_ips.py
@@ -2,17 +2,19 @@ import os
 import glob
 import subprocess
 
-from config import switch_config_dir, ztp_interface
-from config_parsing import get_ip
+from config import switch_config_dir, ztp_interface, ztp_network, ztp_interface_ip
+from config_parsing import get_ip_and_network_port_1
 
-cmds = ""
+cmds = f"addr flush dev {ztp_interface}\n"
+cmds += f"addr add {ztp_interface_ip}/{ztp_network.prefixlen} dev {ztp_interface}\n"
 
-for config_path in glob.glob(f"{switch_config_dir}/*"):
-    ip = get_ip(config_path.split("/")[-1])
-    if ip is None:
+for config_path in glob.glob(f"{switch_config_dir}/*.cfg"):
+    switch_ip, network = get_ip_and_network_port_1(config_path.split("/")[-1].split(".")[0])
+    if switch_ip is None or network is None:
         continue
 
-    cmds += f"addr add {ip} dev {ztp_interface}\n"
+    my_interface_ip = next(network.hosts()).exploded
+
+    cmds += f"addr add {my_interface_ip}/{network.prefixlen} dev {ztp_interface}\n"
 
 subprocess.run(["sudo", "ip", "-batch", "-"], input=cmds.encode(), check=True)
-
--- config_parsing.py
+++ config_parsing.py
@@ -1,17 +1,22 @@
 import re
+from ipaddress import ip_address, ip_network
 
 from config import switch_config_dir
 
 def _read_config_lines(switch_name):
-    config_path = f"{switch_config_dir}/{switch_name}"
+    config_path = f"{switch_config_dir}/{switch_name}.cfg"
     with open(config_path, "r") as f:
         config_lines = [l.strip() for l in f.readlines()]
     return config_lines
 
-def get_ip(switch_name):
+def get_ip_and_network_port_1(switch_name):
     config_lines = _read_config_lines(switch_name)
     for i in range(len(config_lines)):
         if config_lines[i] == "interface MultiGE1/0/1" and not config_lines[i+1].startswith("interface"):
             search_area = " ".join(config_lines[i:i+50])
-            ip = re.search(r"ip address (?P<ip>\d+.\d+.\d+.\d+)", search_area).group("ip")
-            return ip
+            match = re.search(r"ip address (?P<ip>\d+.\d+.\d+.\d+) (?P<netmask>\d+.\d+.\d+.\d+)", search_area)
+            ip = match.group("ip")
+            netmask = match.group("netmask")
+            return ip_address(ip), ip_network(f"{ip}/{netmask}", strict=False)
+
+    return None, None
