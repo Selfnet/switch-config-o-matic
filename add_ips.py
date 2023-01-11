e8e84413ccd0969773a52d4f93c3af5323be4efe
Marco von Rosenberg <marcovr@selfnet.de>
2023-01-11 22:58:30 +0100

    Also add IPs from not-renamed configs to ztp_interface

--- add_ips.py
+++ add_ips.py
@@ -7,7 +7,10 @@ from config_parsing import get_ip_and_network_port_1
 cmds = f"addr flush dev {ztp_interface}\n"
 cmds += f"addr add {ztp_interface_ip}/{ztp_network.prefixlen} dev {ztp_interface}\n"
 
-for config_path in glob.glob(f"{switch_config_dir}/*.cfg"):
+for config_path in glob.glob(f"{switch_config_dir}/*"):
+    if ".ini" in config_path:
+        continue
+
     switch_ip, network = get_ip_and_network_port_1(config_path.split("/")[-1].split(".")[0])
     if switch_ip is None or network is None:
         continue
