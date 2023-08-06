70f5bf95c346861d8da9b8263690a6221dc4e199
Marco von Rosenberg <marcovr@selfnet.de>
2023-08-06 20:21:28 +0200

    Add script to print specific large and small labels

---
+++ print_specific_labels.py
@@ -0,0 +1,23 @@
+import labelprinter.draw
+import labelprinter.printer
+import config
+import os
+import time
+
+
+def main():
+    switch_names = [f.split(".")[0] for f in os.listdir(config.switch_config_dir) if f.endswith(".cfg")]
+    switches_to_print = [n for n in switch_names if False]  # Some condition 
+
+    for name in switches_to_print:
+        small_label = labelprinter.draw.render_small_label(name)
+        labelprinter.printer.print_to_ip(small_label, config.labelprinter_hostname)
+        time.sleep(5)
+
+        large_label = labelprinter.draw.render_text(name, None, None, True)
+        labelprinter.printer.print_to_ip(large_label, config.labelprinter_hostname)
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
